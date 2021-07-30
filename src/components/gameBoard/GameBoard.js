3a8cc66b8f6b88d86223bdce95b9a09abc104569
Kenneth Weaver <kaw0829@gmail.com>
2021-07-30 10:26:12 -0700

    project complete

---
+++ src/components/gameBoard/GameBoard.js
@@ -0,0 +1,15 @@
+import Card from './card/Card';
+import classes from './GameBoard.module.css';
+const GameBoard = ({ cards, clickHandler }) => {
+  return (
+    <div className={classes.board}>
+      {cards.map((card) => {
+        return (
+          <Card key={card.label} picture={card.picture} label={card.label} handler={clickHandler} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default GameBoard;
---
+++ src/components/gameBoard/GameRow/GameRow.js
@@ -0,0 +1,9 @@
+import Card from "../card/Card";
+import classes from 'GameRow.module.css'
+
+const GameRow = () => {
+
+  return (
+    <div className={classes.row}></div>
+  )
+}
